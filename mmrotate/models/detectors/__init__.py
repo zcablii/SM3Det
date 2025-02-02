a2bb6eda39f200cb7f9ceda1e6b859e6728c5559
Yuxuan Li <yuxuan.li.17@ucl.ac.uk>
2025-02-02 21:58:06 +0800

    Update __init__.py

--- mmrotate/models/detectors/__init__.py
+++ mmrotate/models/detectors/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import RotatedBaseDetector
 from .gliding_vertex import GlidingVertex
+from .oriented_rcnn import OrientedRCNN
 from .r3det import R3Det
 from .redet import ReDet
 from .roi_transformer import RoITransformer
